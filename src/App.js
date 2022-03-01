f99c14cd47a1a1a6dcd2868a930a90c183a8c194
jonirenko <93704954+jonirenko@users.noreply.github.com>
2022-03-01 10:13:23 +0200

    koodit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=';
+const API_KEY = 'd4ef271573cd1c22fd264677385bab8a';
 
 function App() {
+  const [eur, setEur] = useState(0);
+  const [gbp, setGbp] = useState(0);
+  const [usd, setUsd] = useState(0);
+  const [gbpRate, setGbpRate] = useState(0);
+  const [usdRate, setUsdRate] = useState(0);
+  
+  async function convert(e) {
+    e.preventDefault();
+    try {
+      const address = URL + API_KEY;
+      const response = await fetch(address);
+
+      if (response.ok) {
+        const json = await response.json();
+        console.log(json.rates.GBP);
+        console.log(json.rates.USD);
+        setGbpRate(json.rates.GBP);
+        setUsdRate(json.rates.USD);
+
+        setUsd(eur * json.rates.USD);
+        setGbp(eur * json.rates.GBP);
+      } else {
+        alert('Error retrieving exchange rate');
+        console.log(response);
+      }
+    } catch (err) {
+      alert(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <form onSubmit={convert}>
+        <div>
+          <label>EUR:</label>&nbsp;
+          <input type="number" step="0.01" value={eur} onChange={e => setEur(e.target.value)} />
+          
+        </div>
+        <div>
+          <label>GBP:</label>&nbsp;
+          <output>{gbp.toFixed(2)} £ Exchange rate: {gbpRate}</output><div></div>
+        </div>
+        <div>
+          <label>USD:</label>&nbsp;
+          <output>{usd.toFixed(2)} $ Exchange rate: {usdRate}</output>
+        </div>
+        <div>
+          <button>Calculate</button>
+        </div>
+      </form>
     </div>
   );
 }
